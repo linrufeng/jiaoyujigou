f0ff10d550890a27da78cc3511b00d3ee2462552
525019506@qq.com <525019506@qq.com>
2019-10-16 00:02:00 +0800

    模块

---
+++ src/view/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="waper">
+        <!-- 登录功能/ 游客 、会员、机构管理者、超级管理员 -->
+        <!-- banner -->
+        <!-- 热门机构 -->
+        <!-- 热门评论 -->
+        <!-- 最新评论 -->
+        <!--首页  机构  个人中心-->
+    </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'	
+
+@Component
+export default class list extends Vue {
+   
+    mounted() {       
+        // (this as any).list = lists
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
